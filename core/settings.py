31d0836f3b0432e9ef153e113b45ce24b5cef7b6
LJlkdskdjflsa <liliangjya@gmail.com>
2021-09-27 08:39:58 +0800

    feat: change setting for daphne

--- core/settings.py
+++ core/settings.py
@@ -132,6 +132,9 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 ASGI_APPLICATION = "core.routing.application"
 CHANNEL_LAYERS = {
     "default": {
-        "BACKEND": "channels.layers.InMemoryChannelLayer",
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [os.environ.get("REDIS_URL", "redis://localhost:6379")],
+        },
     },
 }
